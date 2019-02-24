2c851a2106bfd668a6bc29dda04fd13f1215fd60
Severin Hatt <severinhatt@gmail.com>
2019-02-24 00:45:07 +0000

    board/

--- goalz/urls.py
+++ goalz/urls.py
@@ -24,8 +24,7 @@ from board.views import index
 urlpatterns = [
     # http://localhost:8000/
     path('', index_view, name='index'),
-
     path('admin/', admin.site.urls),
     path('vote/<int:issue_id>', vote),
-    path("board",index),
+    path("board/",index),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
